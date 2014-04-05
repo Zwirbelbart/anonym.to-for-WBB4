d9c3b7b19714e793616423567d8ae77227ef87d1
Zwirbelbart <simon8032000-git@yahoo.de>
2014-04-05 14:50:05 +0200

    introducing new service system

--- src/files/js/linkAnonymizer.js
+++ src/files/js/linkAnonymizer.js
@@ -1,22 +1,40 @@
 var whitelistedLinks = ['#'];
-var serviceUrls = ['http://anonym.to/?', 'http://dontknow.me/at/?'];
-var urlPrefix = serviceUrls[linkAnonymizerService];
+var services = [
+	{
+		'displayName': 'anonym.to',
+		'anonymize': function(link) { 
+			return 'http://anonym.to/?' + encodeURIComponent(link); 
+		}
+	},
+	{
+		'displayName': 'dontknow.me',
+		'anonymize': function(link) { 
+			return 'http://dontknow.me/at/?' + encodeURIComponent(link); 
+		}
+	},
+	{
+		'displayName': 'HideMyAss',
+		'anonymize': function(link) { 
+			return  document.location.protocol + '//hidemyass.com/?' + encodeURIComponent(link); 
+		}
+	}
+]
+var service;
 var regExp = new RegExp("//" + location.host + "($|/)");
 
 function anonymizeLink(elem) {
-	$(elem).attr('href', urlPrefix + encodeURIComponent($(elem).attr('href')));
+	$(elem).attr('href', service.anonymize($(elem).attr('href')));
 	$(elem).attr('data-anonymized', true);
 }
 
 function process() {
-	//get all links that are external bot not marked with externalURL
-	//this selector also ignores links within the message editor.
-	$('*:not(.cke_wysiwyg_div) > a').each(function() {
+	//get all links that are external, ignoring those within the message editor
+	$('*:not(.cke_wysiwyg_div) > a').each(function() {  //<--seperaten commit machen, welcher den invaliden selektor fixt
 		var link = $(this).attr('href');
 
 		if(isExternalAddress(link))
 			$(this).attr('data-external-link', true);
-	
+
 	});
 
 	//anonymize ALL external links now
@@ -29,14 +47,16 @@ function isExternalAddress(link) {
 	if(typeof link == 'undefined' || $.inArray(link, whitelistedLinks) > -1)
 		return false;
 
-	return !((link.substring(0,4) === "http") ? regExp.test(link) : true);
+	return !((link.substring(0, 4) === "http") ? regExp.test(link) : true);
 }
 
 document.addEventListener('DOMContentLoaded', function() { 
-	if(typeof urlPrefix == 'undefined') {
-		console.log('linkAnonymizer: Unable to determine anonymizer service');
+	if(typeof linkAnonymizerService == 'undefined') {
+		console.error('linkAnonymizer: Unable to determine anonymizer service, aborting');
 		return;
 	}
+
+	service = services[linkAnonymizerService];
 
 	$(document).ready(function() {
 		process();
